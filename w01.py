ef4dc3b4e37b41dbf5718e0d01880e2157c98523
weerapan1579 <vivo.976432@gmail.com>
2025-06-18 11:30:12 +0700

    first commit

---
+++ w01.py
@@ -0,0 +1,21 @@
+print("Hello world")
+
+# ตัวแปลจำนวณเต็ม
+number_int ="13"
+
+# ตัวแปรทศนิยม
+number_float = 3.14
+
+
+
+# ตัวแปรข้อความ
+txt = "weerapan sakol"
+
+#  Boolean true/false
+is_online = True
+power_on = False
+
+print = (number_int)
+print = (number_float)
+
+print(type(number_float))
